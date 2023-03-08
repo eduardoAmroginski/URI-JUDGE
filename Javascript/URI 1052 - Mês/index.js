6bc02eea35531afaf1680053904b99635c86aa7e
Eduardo Amroginski Santos <eduardo.amroginski@pearson.com>
2023-03-08 17:38:53 -0300

    Adicionando Exercicio 1052

---
+++ Javascript/URI 1052 - Mês/index.js
@@ -0,0 +1,23 @@
+
+var input = require('fs').readFileSync('dev/stdin', 'utf8');
+var lines = input.split('\n');
+
+let mounths = {
+    1: "January",
+    2: "February",
+    3: "March",
+    4: "April",
+    5: "May",
+    6: "June",
+    7: "July",
+    8: "August",
+    9: "September",
+    10: "October",
+    11: "November",
+    12: "December"
+    }
+
+lines.forEach(value => {
+    value && console.log(mounths[value])
+});
+
